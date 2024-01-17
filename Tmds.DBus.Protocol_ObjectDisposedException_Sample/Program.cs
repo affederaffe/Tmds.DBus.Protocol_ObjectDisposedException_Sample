ed108e39b0ea78499e565409a647d72fcd41373c
affederaffe <68356204+affederaffe@users.noreply.github.com>
2024-01-17 19:01:11 +0100

    Create minimal sample

---
+++ Tmds.DBus.Protocol_ObjectDisposedException_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tmds.DBus.Protocol;
+using Tmds.DBus.SourceGenerator;
+
+
+await Connection.Session.ConnectAsync();
+OrgFreedesktopDBus dbus = new(Connection.Session, "org.freedesktop.DBus", "/org/freedesktop/DBus");
+IDisposable disposable = await dbus.WatchNameOwnerChangedAsync(OnNameChange);
+disposable.Dispose(); // Emits ObjectDisposedException
+
+return;
+
+static void OnNameChange(Exception? e, (string ServiceName, string? OldOwner, string? NewOwner) args)
+{
+    if (e is not null)
+        throw e; // Throws the Exception emitted above
+}
